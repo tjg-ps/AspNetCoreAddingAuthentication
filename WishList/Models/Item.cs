cb2826c8c500ab201c91f4a605daa11c7c499eef
tjg-ps <seagrp01@stepstone.com>
2018-09-10 16:50:48 +0100

    fix missing bracket

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -6,9 +6,8 @@ namespace WishList.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string Description { get; set; }
+        [Required] [MaxLength(50)] public string Description { get; set; }
 
         public virtual ApplicationUser User { get; set; }
+    }
 }
